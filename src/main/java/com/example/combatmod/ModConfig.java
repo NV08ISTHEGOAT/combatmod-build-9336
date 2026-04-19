ae2f55bfc6150617ab339fb8cb6cf98e940de826
NV08ISTHEGOAT <nh.verjans5225@gmail.com>
2026-04-19 16:40:35 +0200

    add src/main/java/com/example/combatmod/ModConfig.java

---
+++ src/main/java/com/example/combatmod/ModConfig.java
@@ -0,0 +1,6 @@
+package com.example.combatmod;
+public class ModConfig {
+    public static boolean invisRevealEnabled = false;
+    public static boolean reachEnabled = false;
+    public static float reachDistance = 20.0f;
+}
