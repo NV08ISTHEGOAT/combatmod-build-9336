a48ab08833f8333d0767a4a4688e7bef7cc2424f
NV08ISTHEGOAT <nh.verjans5225@gmail.com>
2026-04-19 16:40:37 +0200

    add src/main/java/com/example/combatmod/mixin/EntityInvisibilityMixin.java

---
+++ src/main/java/com/example/combatmod/mixin/EntityInvisibilityMixin.java
@@ -0,0 +1,15 @@
+package com.example.combatmod.mixin;
+import com.example.combatmod.ModConfig;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+@Mixin(Entity.class)
+public class EntityInvisibilityMixin {
+    @Inject(method="isInvisibleTo", at=@At("HEAD"), cancellable=true)
+    private void combatmod$revealInvisible(PlayerEntity observer, CallbackInfoReturnable<Boolean> cir){
+        if(ModConfig.invisRevealEnabled) cir.setReturnValue(false);
+    }
+}
